cd0b46e4d9e37517aba874e354ce4d162f650e4c
lqian4 <1994ql1994@163.com>
2018-04-09 17:25:47 -0400

    Version 1

---
+++ Mobile App copy/Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InstructionsController : MonoBehaviour {
+
+	public Text stepText;
+	private int currentStep;
+
+	// Use this for initialization
+	void Start () {
+		currentStep = 0;
+		CurrentInstructionUpdate ();
+
+	}
+
+	public void NextStep() {
+		currentStep++;
+		CurrentInstructionUpdate ();
+	}
+
+	public void PreviousStep() {
+		currentStep--;
+		CurrentInstructionUpdate ();
+
+	}
+
+	private void CurrentInstructionUpdate(){
+		stepText.text = "Step:" + currentStep;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
